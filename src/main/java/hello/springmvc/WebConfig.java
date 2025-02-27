caab45b0dc8bc10165317675fc4aeaa51015adcd
chanyoungcoding <white1614@naver.com>
2025-02-27 16:21:33 +0900

    converter test

--- src/main/java/hello/springmvc/WebConfig.java
+++ src/main/java/hello/springmvc/WebConfig.java
@@ -1,13 +1,21 @@
 package hello.springmvc;
 
 import hello.springmvc.basic.myInterceptor.MyInterceptor;
+import hello.springmvc.myConverter.StringToIntegerConverter;
+import hello.springmvc.myConverter.StringToIpPortConverter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToIntegerConverter());
+        registry.addConverter(new StringToIpPortConverter());
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
--- src/main/java/hello/springmvc/servletError/WebErrorController.java
+++ src/main/java/hello/springmvc/servletError/WebErrorController.java
@@ -1,14 +1,26 @@
 package hello.springmvc.servletError;
 
 import hello.springmvc.apiError.BadRequestException;
+import hello.springmvc.apiError.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+@Slf4j
 @Controller
 public class WebErrorController {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorResult illegalExHandler(IllegalArgumentException ex) {
+        log.error("[exceptionHandler]", ex);
+        return new ErrorResult("BAD", ex.getMessage());
+    };
 
     @GetMapping("/error-ex")
     public void errorEx() {
